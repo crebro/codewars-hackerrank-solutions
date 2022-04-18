e1d15865d9b48a38e46b27361fa5de7f29da6115
crebro <creationduwal@gmail.com>
2022-04-18 22:25:21 +0545

    feat: add hackerrank solutions

---
+++ hackerrank/arrayManipulation.py
@@ -0,0 +1,18 @@
+import math
+
+
+def arrayManipulation(n, queries):
+    v = [0 for i in range(n + 1)]
+    for query in queries:
+        v[query[0] - 1] += query[2]
+        v[query[1]] -= query[2]
+
+    mv = -math.inf
+    li = 0
+    for item in v[0:-1]:
+        val = li + item
+        if val > mv:
+            mv = val
+        li = val
+
+    return mv
---
+++ hackerrank/newYearChaos.py
@@ -0,0 +1,33 @@
+def protectedPrint(value, condition):
+    if condition:
+        print(value)
+
+
+def minimumBribes(q, main=True):
+    n = 0
+    i = 0
+    bribes = {}
+    # op is a bool to check if something is outof place
+    op = False
+    while i < len(q):
+        if i != len(q) - 1 and q[i] > q[i + 1]:
+            q[i], q[i + 1] = q[i + 1], q[i]
+            bribes[q[i + 1]] = bribes[q[i + 1]] + 1 if q[i + 1] in bribes else 1
+            if bribes[q[i + 1]] >= 3:
+                protectedPrint("Too chaotic", main)
+                return
+            n += 1
+        elif q[i] != i + 1:
+            op = True
+
+        i += 1
+
+    if op:
+        mb = minimumBribes(q, main=False)
+        if not (mb):
+            protectedPrint("Too chaotic", main)
+            return
+        protectedPrint(n + mb, main)
+        return n + mb
+    protectedPrint(n, main)
+    return n
